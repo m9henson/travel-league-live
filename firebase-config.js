ad056659c06ed976f49700899ee8def6ba808f76
m9henson <m9henson@gmail.com>
2026-08-05 09:50:03 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,19 @@
 // Replace this object with your Firebase web app configuration.
 // You can also leave it as-is and paste the config into the setup screen.
-export const firebaseConfig = null;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrhrQrHl_-ax7Y1fBl54hTCtMeA-pRnMw",
+  authDomain: "travel-league-live.firebaseapp.com",
+  projectId: "travel-league-live",
+  storageBucket: "travel-league-live.firebasestorage.app",
+  messagingSenderId: "1020980600179",
+  appId: "1:1020980600179:web:4d9afba0ec90996eb2f906"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
